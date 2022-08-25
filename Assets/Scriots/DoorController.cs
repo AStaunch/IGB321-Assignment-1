504c54d7525b9be085f7132009d8c19ac61fa5ac
AStaunch <n10481320@qut.edu.au>
2022-08-25 16:47:16 +1000

    Update

---
+++ Assets/Scriots/DoorController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour, iTrigger
+{
+    private bool isOpen = false;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.F))
+        {
+            Activate();
+        }
+    }
+    public void Activate()
+    {
+        GetComponent<Animator>().SetTrigger("Activate");
+    }
+}
---
+++ Assets/Scriots/PlayerControls.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Player Controls
+        if (Input.GetKeyDown("f")) {
+            Interact();
+        }
+		
+	}
+
+
+    void Interact() {
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 10.0f)){
+
+            Debug.DrawLine(transform.position, hit.point);
+
+            if (hit.transform.gameObject.GetComponent<iTrigger>() != null) {
+                hit.transform.gameObject.GetComponent<iTrigger>().Activate();
+            }
+        }
+    }
+}
+    
